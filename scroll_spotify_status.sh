c4ecdd7fb564a971009b9dbbc661f2280077f0bc
Felipe Marcelino <felipe.marcelino1991@gmail.com>
2024-01-04 02:16:27 -0300

    polybar-spotify changed

--- scroll_spotify_status.sh
+++ scroll_spotify_status.sh
@@ -4,10 +4,10 @@
 zscroll -l 30 \
         --delay 0.1 \
         --scroll-padding "  " \
-        --match-command "`dirname $0`/get_spotify_status.sh --status" \
+        --match-command "$XDG_POLYBAR/cuts/scripts/scripts/polybar-spotify/get_spotify_status.sh --status" \
         --match-text "Playing" "--scroll 1" \
         --match-text "Paused" "--scroll 0" \
-        --update-check true "`dirname $0`/get_spotify_status.sh" &
+        --update-check true "$XDG_POLYBAR/cuts/scripts/scripts/polybar-spotify/get_spotify_status.sh" &
 
 wait
 
